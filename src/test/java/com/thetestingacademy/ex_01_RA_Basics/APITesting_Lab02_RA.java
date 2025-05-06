12923c76877d72486a3ff833c9cfde2d7b8403b6
Divyaniurkude2 <yournewemail@example.com>
2025-05-06 18:38:11 +0530

    06 may

---
+++ src/test/java/com/thetestingacademy/ex_01_RA_Basics/APITesting_Lab02_RA.java
@@ -0,0 +1,23 @@
+package com.thetestingacademy.ex_01_RA_Basics;
+
+import io.restassured.RestAssured;
+
+import java.util.Scanner;
+
+public class APITesting_Lab02_RA {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the pincode");
+        String pincode = sc.next();
+
+        RestAssured.given()
+                .baseUri("https://api.zippopotam.us")
+                .basePath("/IN/"+ pincode)
+                .when()
+                .get().then().log().all().statusCode(200);
+
+        //Headers, Cookies, Response body, ....others.
+    }
+}
